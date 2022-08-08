98382229a302288876a531947db4f6602287b99a
EGYuda23 <egyyudanugraha@gmail.com>
2022-08-08 21:26:29 +0700

    fix sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,10 +20,20 @@
     <div id="app">
         <div id="sidebar" class="active">
             <div class="sidebar-wrapper active">
+                <div class="sidebar-header position-relative">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
+                            <div class="form-check form-switch fs-6">
+                                <input class="form-check-input  me-0" type="checkbox" id="toggle-dark" hidden>
+                            </div>
+                        </div>
+                        <div class="sidebar-toggler  x">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
                 <div class="sidebar-menu">
                     <ul class="menu">
-                        <li class="sidebar-title">Menu</li>
-                        
                         <li
                             class="sidebar-item active ">
                             <a href="/" class='sidebar-link'>
