a6a5cce9f0a544b227fdf4954871e0ed3a916250
EGYuda23 <egyyudanugraha@gmail.com>
2022-08-08 13:35:11 +0700

    add view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,6 +23,38 @@
     <div id="app">
         <div id="main" class="layout-horizontal">
             <header class="mb-5">
+            <div class="header-top">
+                    <div class="container">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/images/logo/logo.svg" alt="Logo"></a>
+                        </div>
+                        <div class="header-top-right">
+
+                            <div class="dropdown">
+                                <a href="#" id="topbarUserDropdown" class="user-dropdown d-flex align-items-center dropend dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div class="avatar avatar-md2" >
+                                        <img src="assets/images/faces/1.jpg" alt="Avatar">
+                                    </div>
+                                    <div class="text">
+                                        <h6 class="user-dropdown-name">John Ducky</h6>
+                                        <p class="user-dropdown-status text-sm text-muted">Member</p>
+                                    </div>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end shadow-lg" aria-labelledby="topbarUserDropdown">
+                                  <li><a class="dropdown-item" href="#">My Account</a></li>
+                                  <li><a class="dropdown-item" href="#">Settings</a></li>
+                                  <li><hr class="dropdown-divider"></li>
+                                  <li><a class="dropdown-item" href="auth-login.html">Logout</a></li>
+                                </ul>
+                            </div>
+
+                            <!-- Burger button responsive -->
+                            <a href="#" class="burger-btn d-block d-xl-none">
+                                <i class="bi bi-justify fs-3"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <nav class="main-navbar">
                     <div class="container">
                         <ul>
@@ -34,14 +66,20 @@
                             </li>
                             <li
                                 class="menu-item  ">
+                                <a href="/matakuliah" class='menu-link'>
+                                    <span>Matakuliah</span>
+                                </a>
+                            </li>
+                            <li
+                                class="menu-item  ">
                                 <a href="/mahasiswa" class='menu-link'>
                                     <span>Mahasiswa</span>
                                 </a>
                             </li>
                             <li
                                 class="menu-item  ">
-                                <a href="/matakuliah" class='menu-link'>
-                                    <span>Matakuliah</span>
+                                <a href="/semester" class='menu-link'>
+                                    <span>Semester</span>
                                 </a>
                             </li>
                             <li
@@ -52,20 +90,14 @@
                             </li>
                             <li
                                 class="menu-item  ">
-                                <a href="/kontrak" class='menu-link'>
-                                    <span>Kontrak</span>
-                                </a>
-                            </li>
-                            <li
-                                class="menu-item  ">
                                 <a href="/jadwal" class='menu-link'>
                                     <span>Jadwal</span>
                                 </a>
                             </li>
                             <li
                                 class="menu-item  ">
-                                <a href="/semester" class='menu-link'>
-                                    <span>Semester</span>
+                                <a href="/kontrak" class='menu-link'>
+                                    <span>Kontrak</span>
                                 </a>
                             </li>
                         </ul>
@@ -75,10 +107,6 @@
             </header>
 
             <div class="content-wrapper container">
-                
-                <div class="page-heading">
-                    <h3>Selamat datang!</h3>
-                </div>
                 <div class="page-content">
                     <section class="row">
                         @yield('content')
